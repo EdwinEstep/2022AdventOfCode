9e668a7b8f6aabfe4fc6cfcb66d77824153ae73c
EdwinEstep <edwin.estep@gmail.com>
2022-12-03 09:56:50 -0600

    Completed day3 part 2. Used bitwise-and to find common elements between elf backpacks.

--- Day03/part2/src/main.rs
+++ Day03/part2/src/main.rs
@@ -1,3 +1,99 @@
-fn main() {
-    println!("Hello, world!");
+// `cargo run -- search_text ../input.txt`
+
+// args
+use std::env;
+
+// files
+use std::fs::File;
+use std::io::{prelude::*, BufReader}; // for reading line-by-line
+
+
+
+const ELVES_PER_GROUP: usize = 3;
+
+
+fn main() -> std::io::Result<()> {
+    // get file name from input args
+    let args: Vec<String> = env::args().collect();
+    let fpath = &args[1];
+
+    // open file
+    let file = File::open(fpath)?;
+    let reader = BufReader::new(file);  
+
+    let mut elf_num = 0;         // track which elf in each group
+    let mut chars_used: [u64; ELVES_PER_GROUP] = [0; ELVES_PER_GROUP]; // bits correspond to priorities that have been used.
+    let mut shared_chars: u64 = 0;
+
+    let mut score: u64 = 0;
+
+
+    // read line by line
+    for line in reader.lines() {
+        // make a clone of the line, since the for-loop has ownership
+        // then, get an iterable of the characters in the string
+        let lstr = line.unwrap().clone(); 
+        let char_iter = lstr.chars();
+
+
+        for c in char_iter {
+            let mut priority = [0; 1];
+            
+            // essentially extract ASCII rep and shift down
+            if c.is_ascii_uppercase() {
+                c.encode_utf8(&mut priority);
+                priority[0] = priority[0] - 38;
+            }
+            else if c.is_ascii_lowercase() {
+                c.encode_utf8(&mut priority);
+                priority[0] = priority[0] - 96;
+            }
+
+            // set bit corresponding to character received
+            chars_used[elf_num] = chars_used[elf_num] | (1 << priority[0]);
+            print!("{} ", priority[0]);
+        }
+        println!();
+
+
+        // track which group & which elfwe're handling
+        elf_num += 1;
+        if elf_num >= ELVES_PER_GROUP {
+            // we're at the end of the group, so compare the bitmaps
+            // the cool thing here is that the priority is EXACTLY equal
+            // to the number of trailing zeros.
+            shared_chars = chars_used[0];
+            for i in 1..ELVES_PER_GROUP {
+                shared_chars &= chars_used[i];
+            }
+
+            score += u64::from(shared_chars.trailing_zeros());
+
+            for i in 0..ELVES_PER_GROUP {
+                println!("{:#064b}", chars_used[i]);
+            }
+            println!("{:#064b}", shared_chars);
+            println!("{}\n", shared_chars.trailing_zeros());
+
+
+            // reset data and move to next group
+            elf_num = 0;
+            chars_used = [0; ELVES_PER_GROUP];
+        }
+    }
+
+
+    // print result
+    println!("score:  {}", score);
+    Ok(())
 }
+
+
+/*
+IDEA:  Use stuff from counting sort
+
+We do a similar thing to part 1, except we need to collect bitmaps for N elves at a time.
+Then, just bitwise-and them together, and we're left with the one value shared among all of them.
+
+The priority of this value is just the number of trailing zeros.
+*/
